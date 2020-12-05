2c82659c1e5e98b91ba2842ac516582388712b87
barely-a-developer <19259418+barely-a-developer@users.noreply.github.com>
2020-12-05 11:46:37 -0500

    Initial structure

---
+++ src/main/java/notes/notesapigw/NotesApigwApplication.java
@@ -0,0 +1,13 @@
+package notes.notesapigw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesApigwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesApigwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/notes/notesapigw/NotesApigwApplicationTests.java
@@ -0,0 +1,13 @@
+package notes.notesapigw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApigwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
